f2c25793f3c3893d70a2f8860cb6a482b6b23885
kjolly <karenshilohj@gmail.com>
2020-05-24 19:04:43 -0700

    Adding homework2

---
+++ Lesson02/calculator.js
@@ -0,0 +1,26 @@
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+
+console.log('Let us do some math');
+console.log('here are some math problems');
+const mult = num1*num2;
+console.log('This is 1*2');
+console.log(mult);
+
+let sum = num2 + num3;
+console.log('Sum of two numbers')
+console.log(sum);
+console.log('now add another number');
+sum = num2 + num3 + num4;
+console.log(sum);
+
+let divided = num4/num2;
+
+console.log('divided nums');
+console.log(divided);
+
+let subtracted = num4 - num1;
+console.log('doing some subtration');
+console.log(subtracted);
